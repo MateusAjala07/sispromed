84c982bbb693e261999936753bf7d05edafd3fd0
comsysteminformatica <comsysteminf@gmail.com>
2026-02-11 09:46:21 -0400

    test: Criação para verificar valores minimos input e estado botão entrar

--- src/tests/Login.spec.tsx
+++ src/tests/Login.spec.tsx
@@ -1,38 +1,58 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
-import { describe, it, expect, vi, beforeEach } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import Login from "@/pages/Login";
-import userEvent from "@testing-library/user-event";
 
-describe("Login", () => {
-  beforeEach(() => {
-    render(
-      <MemoryRouter>
-        <Login />
-      </MemoryRouter>
-    );
+beforeEach(() => {
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+});
+
+describe("Verifica botão entrar", () => {
+  it("Deve ficar desativado quando carregando", () => {
+    const inputUsuario = screen.getByLabelText(/usuário/i);
+    const inputSenha = screen.getByLabelText(/senha/i);
+    const button = screen.getByRole("button", { name: /entrar/i });
+
+    fireEvent.change(inputUsuario, { target: { value: "teste" } });
+    fireEvent.change(inputSenha, { target: { value: "12345678" } });
+    fireEvent.click(button);
+
+    expect(button).toBeDisabled();
   });
+});
 
-  it("Renderiza o titulo", () => {
-    expect(screen.getByText("Bem-vindo")).toBeInTheDocument();
-  });
-
-  it("Botão Entrar existe", () => {
-    expect(screen.getByRole("button", { name: /entrar/i })).toBeInTheDocument();
-  });  
-
-  it("Mostra carregando ao submeter", async () => {
-    const user = userEvent.setup();
-
-    await user.type(screen.getByLabelText(/usuário/i), "teste");
-    await user.type(screen.getByLabelText(/senha/i), "12345678");
-    const button = screen.getByRole("button", {
+describe("Verifica inputs", () => {
+  it("Deve mostrar mensagem de erro no input Usuário por conter caracteres menor que o mínimo", async () => {
+    const botao = screen.getByRole("button", {
       name: /entrar/i,
     });
+    const input = screen.getByLabelText(/usuário/i);
 
-    await user.click(button);
+    fireEvent.change(input, { target: { value: "11" } });
+    fireEvent.click(botao);
 
-    expect(button).toHaveTextContent(/carregando.../i);
+    const mensagem = await screen.findByText(
+      "O nome deve conter no mínimo 3 caracteres"
+    );
+
+    expect(mensagem).toBeInTheDocument();
   });
 
+  it("Deve mostrar mensagem de erro no input Senha por conter caracteres menor que o mínimo", async () => {
+    const botao = screen.getByRole("button", { name: /entrar/i });
+    const input = screen.getByLabelText(/senha/i);
+
+    fireEvent.change(input, { target: { value: "1111111" } });
+    fireEvent.click(botao);
+
+    const mensagem = await screen.findByText(
+      "A senha deve conter no mínimo 8 caracteres"
+    );
+
+    expect(mensagem).toBeInTheDocument();
+  });
 });
